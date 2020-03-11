de95409570347c37ce2497445193fd588db3bbbf
akino102819 <k.akino1998@icloud.com>
2020-03-11 16:03:31 +0900

    ログイン機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,9 @@
 class TweetsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   def index
     @tweets = Tweet.all
   end
+
   def new
     @tweet = Tweet.new
   end
@@ -9,8 +11,15 @@ class TweetsController < ApplicationController
   def create
     Tweet.create(tweet_params)
   end
+
+  def show
+  end
+
   private
   def tweet_params
-    params.require(:tweet).permit(:title, :image, :content)
+    params.require(:tweet).permit(:title, :image, :content).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
- root "tweets#index"
- resources :tweets, only: [:new, :create]
+  devise_for :users
+  root "tweets#index"
+  resources :tweets, only: [:index, :new, :create]
+  resources :users, only: :show
 end
