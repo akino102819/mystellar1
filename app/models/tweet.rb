a9f2873a56562cb781d2fec0b9de37a9db3d1015
akino102819 <k.akino1998@icloud.com>
2020-04-21 22:42:00 +0900

    カテゴリー機能導入

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,6 @@ class Tweet < ApplicationRecord
   validates :content, presence: true
   
   belongs_to :user
-
+  belongs_to :category
   has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root "tweets#index"
   resources :tweets do
     resources :comments, only: :create
   end
+  
   resources :users, only: [:show]
+  resources :categories
 end
