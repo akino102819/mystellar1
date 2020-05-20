030a4e85504022c00d87fc55ace2d8477a64fce7
akino102819 <k.akino1998@icloud.com>
2020-05-20 10:07:00 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.12.1"
+lock "~> 3.14.0"
 
 set :application, "mystellar1"
 set :repo_url, "https://github.com/akino102819/mystellar1.git"
